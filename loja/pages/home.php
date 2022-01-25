cdeafd41c38c7e0a4c9070abfc2f38acd1476734
André Lucas Silveira Dutra <andre1234321a@gmail.com>
2022-01-25 16:04:35 -0300

    Add files via upload

---
+++ loja/pages/home.php
@@ -0,0 +1,126 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Loja - home</title>
+
+  <!-- LINKS -->
+
+  <link rel="stylesheet" href="../css/main.css">
+</head>
+
+<body>
+
+  <!-- navbar -->
+  <header class="header" id="header">
+    <nav class="nav container">
+      <a href="index.html" class="nav__logo"><img src="./icons/favicon.ico" alt="LOGO" width="40px"></a>
+
+      <div class="nav__menu" id="nav-menu">
+        <ul class="nav__list">
+          <li class="nav__item">
+            <a href="#home" class="nav__link"><i class="fas fa-home"></i> Home </a>
+          </li>
+          <li class="nav__item">
+            <a href="maissobre.html" class="nav__link"><i class="fa fa-user" aria-hidden="true"></i> About </a>
+          </li>
+          <li class="nav__item">
+            <a href="#portfolio" class="nav__link"><i class="fa fa-folder-open" aria-hidden="true"></i> 
+              Portfolio</a>
+          </li>
+          <li class="nav__item">
+            <a href="#contact" class="nav__link"><i class="fas fa-envelope-open"></i> Contact</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="main-header">
+
+        <div class="main-header-1">
+
+          <li class="header-butons">
+            <a class="itens-main-header" href="contato.php">Contato</a>
+          </li>
+
+          <li class="header-butons">
+            <a class="itens-main-header" href="contato.php">Contato</a>
+          </li>
+
+          <li class="header-butons">
+            <a class="itens-main-header" href="contato.php">Contato</a>
+          </li>
+
+        </div>
+
+      <div class="nav__toggle" id="nav-toggle">
+      <img src="../icons/icone-menu.png" alt="MENU" height="60px">
+      </div>
+
+
+    </nav>
+  </header>
+
+  <main>
+
+
+    <!-- HOME -->
+    <section class="home-inicial" id="home">
+
+
+      <div class="home-container container grid">
+
+
+        <div class="conteudo-inicial">
+          <div class="inicial-text">
+            <h1>Produtos confeccionados sob medida<span>.</span></h1>
+            <p>Bicicletas elétricas de alta precisão e qualidade, feitas sob medida para você. Explore o mundo na sua velocidade com a Bikcraft.</p>
+            <a class="botao" href="">veja mais dessas</a>
+          </div>
+
+          <div class="img-inicial">
+            <img src="../img/introducao.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- <section class="vitrine">
+
+      <div class="carousel">
+
+        <div class="list-produtos">
+          <div class="produto">
+            <img src="" alt="">
+            <h2>nome</h2>
+            <h2>preco</h2>
+            <a href="#">comprar</a>
+          </div>
+      
+
+        </div>
+
+      </div>
+    </section> -->
+
+
+
+
+  </main>
+
+
+
+
+<script src="../js/main.js"></script>
+
+</body>
+
+</html>
